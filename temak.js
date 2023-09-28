f32e045242d6b150671d8c0f50f1f70ef4c8a49b
borpince <admin@pince.eu>
2023-09-29 01:22:20 +0200

    új tartalom

--- temak.js
+++ temak.js
@@ -18,7 +18,7 @@ var temak =
       "2020": [{cim:"№ 1: cabernet sauvignon", nev:"01", ver:0, kelt:"2021-10-26"}],
       "2022": [
         {cim:"№ 2: oportó",nev:"02",ver:"2023-05-12", kelt:"2022-10-14"},
-        {cim:"№ 3: rozé",nev:"03", ver:0, kelt:"2022-10-14"},
+        {cim:"№ 3: rozé",nev:"03", ver:"2023-09-25", kelt:"2022-10-14"},
         {cim:"№ 4: kékfrankos",nev:"04",ver:"2023-07-23", kelt:"2022-10-14"}
       ],
       "2023": [{cim:"№ 5: tramini", nev:"05", ver:0, kelt:"2023-09-29"}]
