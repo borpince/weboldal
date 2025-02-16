5b5e79a747c78e845d004daf182bd9e123269b63
borpince <admin@pince.eu>
2025-02-16 10:05:29 +0100

    új fejezet

--- temak.js
+++ temak.js
@@ -114,7 +114,8 @@ var temak =
         {cim:"mágikus vonzerő", subfolder:"/velemeny", nev:"magus", ver:0, kelt:"2024-12-06"},
         {cim:"a jog nem egzakt tudomány", subfolder:"/velemeny", nev:"egzakt", ver:0, kelt:"2024-12-18"},
         {cim:"jesus bleibet meine freude", subfolder:"/velemeny", nev:"fantazia", ver:0, kelt:"2025-01-16"},
-        {cim:"életünk és a bor", subfolder:"/velemeny", nev:"hamvas", ver:0, kelt:"2025-02-01"}
+        {cim:"életünk és a bor", subfolder:"/velemeny", nev:"hamvas", ver:0, kelt:"2025-02-01"},
+        {cim:"milyen hülyének lenni?", subfolder:"/velemeny", nev:"hulye", ver:0, kelt:"2025-02-16"}
       ],
       "d": [
         {cim:"borok, versenyek", subfolder:"/bv", nev:"borverseny", ver:0, kelt:"2023-04-18"},
