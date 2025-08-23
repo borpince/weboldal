52f7fb04ad0b9d9924a972a1c859c15e03d380e5
borpince <admin@pince.eu>
2025-08-23 16:05:04 +0200

    "?sima" paraméter

--- fejezetek.js
+++ fejezetek.js
@@ -513,14 +513,15 @@
       });
     } //! 514 hamburger.html
     
-    if ((glob.href_nev != "naptar") && (glob.href_nev != "naplo") && (glob.href_nev != "katalogus") && letezik(glob.href_nev,false).tortenet)
+    var sima = (window.location.search.substring(1).trim() == "sima");
+    if (!sima && (glob.href_nev != "naptar") && (glob.href_nev != "naplo") && (glob.href_nev != "katalogus") && letezik(glob.href_nev,false).tortenet)
       hamar = setTimeout(function() {
         if (left_side[0]) {
           left_side[0].style.top = eltuntet_y;
           latszik = !latszik;
         }
       },100);
-    
+
     legujabb_tortenetek("ajanlo");
     legujabb_tortenetek("bovult");
     //pontatlan page jump igazítás:
