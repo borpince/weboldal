bf5b94519abc6f95176753c1a509d9d2c678db2c
borpince <admin@pince.eu>
2026-01-27 04:08:40 +0100

    új fejezet

--- temak.js
+++ temak.js
@@ -202,7 +202,8 @@ var temak =
         {cim:"köntörfal", subfolder: "/ai", nev:"kontorfal", ver:0, kelt:"2024-11-09"},
         {cim:"korhatár-besorolás", subfolder:"/ai", nev:"korhatar", ver:0, kelt:"2025-09-14"},
         {cim:"AI-strici", subfolder:"/ai", nev:"ai-strici", ver:0, kelt:"2026-01-24"},
-        {cim:"a zsíroskenyér legendája", subfolder:"/ai", nev:"zsiroskenyer", ver:0, kelt:"2026-01-26"}
+        {cim:"a zsíroskenyér legendája", subfolder:"/ai", nev:"zsiroskenyer", ver:0, kelt:"2026-01-26"},
+        {cim:"bekapcsolva maradt a puncsolós flag", subfolder:"/ai", nev:"puncs-flag", ver:0, kelt:"2026-01-27"}
       ],
       "f": [
         {cim:"IT-kaland", subfolder:"/kaland", nev:"kaland", ver:0, kelt:"2022-11-28"},
